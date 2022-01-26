7ad5ce0456e32f5c359c44c7ea91e4c825200bd5
Thomas Caron <thomas.caron@polymtl.ca>
2022-01-26 11:26:21 -0500

    Added new assets

--- TP1/algorithms/LZ77.py
+++ TP1/algorithms/LZ77.py
@@ -1,82 +1,163 @@
+# Code from https://github.com/manassra/LZ77-Compressor
+
+import math
+from bitarray import bitarray
+
 class LZ77:
-    def __init__(self, message, dictSize = 6, lookAhead = 32) -> None:
-        self.message = message
-        self.dictSize = dictSize
-        self.triplets = []
-        self.lookAhead = lookAhead
-    # def encode(self):
-    #     idx = 0
-    #     while idx < len(self.message):
-    #         decalage = 0
-    #         symb = self.message[max(0, idx - self.dictSize): idx]
-    #         if len(symb) < self.dictSize:
-    #             decalage = self.dictSize - len(symb)
-            
+	"""
+	A simplified implementation of the LZ77 Compression Algorithm
+	"""
+	MAX_WINDOW_SIZE = 400
+
+	def __init__(self, window_size=20, lookahead_size=15):
+		self.window_size = min(window_size, self.MAX_WINDOW_SIZE) 
+		self.lookahead_buffer_size = lookahead_size # length of match is at most 4 bits
+
+	def compress(self, data: bytes, output_file_path=None, verbose=False):
+		"""
+		Given the path of an input file, its content is compressed by applying a simple 
+		LZ77 compression algorithm. 
+		The compressed format is:
+		0 bit followed by 8 bits (1 byte character) when there are no previous matches
+			within window
+		1 bit followed by 12 bits pointer (distance to the start of the match from the 
+			current position) and 4 bits (length of the match)
+		
+		If a path to the output file is provided, the compressed data is written into 
+		a binary file. Otherwise, it is returned as a bitarray
+		if verbose is enabled, the compression description is printed to standard output
+		"""
+		i = 0
+		output_buffer = bitarray(endian='big')
+
+		while i < len(data):
+			#print(i)
+
+			match = self.findLongestMatch(data, i)
+
+			if match: 
+				# Add 1 bit flag, followed by 12 bit for distance, and 4 bit for the length
+				# of the match 
+				(bestMatchDistance, bestMatchLength) = match
+
+				output_buffer.append(True)
+				output_buffer.frombytes(bytes([bestMatchDistance >> 4]))
+				output_buffer.frombytes(bytes([((bestMatchDistance & 0xf) << 4) | bestMatchLength]))
+
+				if verbose:
+					print("<1, %i, %i>" % (bestMatchDistance, bestMatchLength), end='')
+				
+				i += bestMatchLength
+			else:
+				# No useful match was found. Add 0 bit flag, followed by 8 bit for the character
+				output_buffer.append(False)
+				output_buffer.frombytes(bytes([data[i]]))
+				
+				if verbose:
+					print("<0, %s>" % data[i], end='')
+
+				i += 1
+
+		# fill the buffer with zeros if the number of bits is not a multiple of 8		
+		output_buffer.fill()
+
+		# write the compressed data into a binary file if a path is provided
+		if output_file_path:
+			try:
+				with open(output_file_path, 'wb') as output_file:
+					output_file.write(output_buffer.tobytes())
+					print("File was compressed successfully and saved to output path ...")
+					return None
+			except IOError:
+				print('Could not write to output file path. Please check if the path is correct ...')
+				raise
+
+		# an output file path was not provided, return the compressed data
+		return output_buffer
 
 
-    def encode(self):
-        index = 0  # Position courante dans le message
-        while index < len(self.message):
-            decalage = 0
-            lookahead = self.lookAhead
-            symbDict = self.message[max(0, index - self.dictSize): index]  # Symboles disponibles du dictionnaire
-            if len(symbDict) < self.dictSize:
-                decalage = self.dictSize - len(symbDict) #Pour l'ajustement des indices de position quand le dictionnaire n'est pas plein.
-            # If we have long strings, we don't want to check for the whole string
-            # Implemented lookahead
-            if index + lookahead > len(self.message):
-                lookahead = len(self.message) - index
+	def decompress(self, input_file_path, output_file_path=None):
+		"""
+		Given a string of the compressed file path, the data is decompressed back to its 
+		original form, and written into the output file path if provided. If no output 
+		file path is provided, the decompressed data is returned as a string
+		"""
+		data = bitarray(endian='big')
+		output_buffer = []
 
-            sousChaine = self.message[index:index+lookahead] #Le dernier caractère sera ajouté comme 3e élément du dernier triplet
+		# read the input file
+		try:
+			with open(input_file_path, 'rb') as input_file:
+				data.fromfile(input_file)
+		except IOError:
+			print('Could not open input file ...')
+			raise
 
-            # On cherche la sous-chaine la plus longue
-            pos, length = 0, 0
-            while len(sousChaine) > 0:
-                if sousChaine in symbDict:
-                    pos, length = symbDict.find(sousChaine), len(sousChaine) #dernière occurrence si plusieurs choix
-                    break
-                sousChaine = sousChaine[:-1] # On a pas trouvé, donc on enlève un élément.
-            
-            # Regarde ensuite si on peut allonger la sous-chaine trouvée après la position de l'index: AB|ABABABABAB
-            increment = 0
-            while length > 0 \
-                    and index+length+increment < (len(self.message)-1) \
-                    and self.message[index-len(symbDict)+pos+length+increment] == self.message[index+length+increment]:
-                increment += 1
-            length += increment
+		while len(data) >= 9:
 
-            # Enregistrement des triplets
-            c = self.message[index + length]  #Caractère suivant non encodé.
+			flag = data.pop(0)
 
-            if length == 0:
-                pos = 0
-            else:
-                pos = pos + decalage #Pour l'ajustement des indices de position quand le dictionnaire n'est pas plein.
+			if not flag:
+				byte = data[0:8].tobytes()
 
-            self.triplets.append((pos, length, c))
+				output_buffer.append(byte)
+				del data[0:8]
+			else:
+				byte1 = ord(data[0:8].tobytes())
+				byte2 = ord(data[8:16].tobytes())
 
-            index += max(length+1, 1)  # Avance la position dans le message
-        
-        return self.triplets
+				del data[0:16]
+				distance = (byte1 << 4) | (byte2 >> 4)
+				length = (byte2 & 0xf)
 
-    def decode(self):
-        decoded = ""
+				for i in range(length):
+					output_buffer.append(output_buffer[-distance])
+		out_data =  b''.join(output_buffer)
 
-        for triplet in self.triplets:
-            pos, length, char = triplet[0], triplet[1], triplet[2]
-            if (pos, length) == (0, 0):
-                decoded += char
-            else:
-                decalage = 0
-                if len(decoded) < self.dictSize:
-                    decalage = self.dictSize - len(decoded) #Pour l'ajustement des indices de position quand le dictionnaire n'est pas plein.
-                start = max(0, len(decoded) - self.dictSize)
-                for i in range(length):
-                    decoded += decoded[start+pos-decalage+i]
-                decoded += char
+		if output_file_path:
+			try:
+				with open(output_file_path, 'wb') as output_file:
+					output_file.write(out_data)
+					print('File was decompressed successfully and saved to output path ...')
+					return None 
+			except IOError:
+				print('Could not write to output file path. Please check if the path is correct ...')
+				raise 
+		return out_data
 
-        return decoded
 
-    def compression_rate(self) -> float:
-        print(len(self.triplets))
-        return 1 - (len(self.triplets) * 3) / len(self.message)
+	def findLongestMatch(self, data, current_position):
+		""" 
+		Finds the longest match to a substring starting at the current_position 
+		in the lookahead buffer from the history window
+		"""
+		end_of_buffer = min(current_position + self.lookahead_buffer_size, len(data) + 1)
+
+		best_match_distance = -1
+		best_match_length = -1
+
+		# Optimization: Only consider substrings of length 2 and greater, and just 
+		# output any substring of length 1 (8 bits uncompressed is better than 13 bits
+		# for the flag, distance, and length)
+		for j in range(current_position + 2, end_of_buffer):
+
+			start_index = max(0, current_position - self.window_size)
+			substring = data[current_position:j]
+
+			for i in range(start_index, current_position):
+
+				repetitions = len(substring) // (current_position - i)
+
+				last = len(substring) % (current_position - i)
+
+				matched_string = data[i:current_position] * repetitions + data[i:i+last]
+
+				if matched_string == substring and len(substring) > best_match_length:
+					best_match_distance = current_position - i 
+					best_match_length = len(substring)
+
+		if best_match_distance > 0 and best_match_length > 0:
+			return (best_match_distance, best_match_length)
+		return None
+
+
--- TP1/algorithms/RLE.py
+++ TP1/algorithms/RLE.py
@@ -1,44 +1,48 @@
-from typing import Literal, Union
+import struct
+import numpy as np
 
+from bitarray import bitarray
 
 class RLE:
-    def __init__(self, message: Union[str, bytes], counterSize: int = 8, type: Literal["text", "binary"] = "text") -> None:
+    def __init__(self, message: bytes, counterSize: int = 8) -> None:
         self.message = message
-        self.encoded = []
         self.counterSize = counterSize
         self.binaryMap = { self.message[0]: "{0:b}".format(0).zfill(self.counterSize) }
-        self.type = type
 
-    def encode(self) -> list:
+    def compress(self, message: bytes, output_file_path=None) -> list:
         result = []
-        for i, item in enumerate(self.message):
-            if item not in self.binaryMap:
-                self.binaryMap[item] = "{0:b}".format(len(self.binaryMap)).zfill(self.counterSize)
+        for i in range(len(message)):
+            if message[i] not in self.binaryMap:
+                self.binaryMap[message[i]] = "{0:b}".format(len(self.binaryMap)).zfill(self.counterSize)
         
         count = 0
-        for i, item in enumerate(self.message):
+        for i in range(len(message)):
             if i == 0:
                 # Ignore first element since its already present, start counter at 0
                 continue
             if count >= 2 ** self.counterSize:
                 # Reset counter
-                result.extend(["{0:b}".format(count - 1).zfill(self.counterSize), self.binaryMap[self.message[i - 1]]])
+                result.extend(["{0:b}".format(count - 1).zfill(self.counterSize), self.binaryMap[message[i - 1]]])
                 count = 0
-            if i < len(self.message) and item == self.message[i - 1]:
+            if i < len(message) and message[i] == message[i - 1]:
                 count += 1
             else:
-                result.extend(["{0:b}".format(count).zfill(self.counterSize), self.binaryMap[self.message[i - 1]]])
+                result.extend(["{0:b}".format(count).zfill(self.counterSize), self.binaryMap[message[i - 1]]])
                 count = 0
-        result.extend(["{0:b}".format(count).zfill(self.counterSize), self.binaryMap[self.message[-1]]])
-        self.encoded = result
+        result.extend(["{0:b}".format(count).zfill(self.counterSize), self.binaryMap[message[-1]]])
+        
+        if output_file_path:
+            try:
+                with open(output_file_path, 'wb') as output_file:
+                    filedata = [int(item, 2) for item in result]
+
+                    output_file.write(bytearray(filedata))
+                    print("File was compressed successfully and saved to output path ...")
+            except IOError:
+                print('Could not write to output file path. Please check if the path is correct ...')
+                raise
 
         return result
-
-    def _encode_binary(self):
-        pass
-
-    def _encode_text(self):
-        pass
 
     def message_as_binary(self) -> str:
         result = ""
@@ -46,12 +50,29 @@ class RLE:
             result += self.binaryMap[item]
         return result
 
-    def decode(self):
+    def decompress(self, input_file_path, output_file_path = None) -> bytes:
         result = []
+        dtype = np.dtype('B')
+        try:
+            with open(input_file_path, 'rb') as input_file:
+                data = np.fromfile(input_file, dtype)
+        except IOError:
+            print('Could not open input file ...')
+            raise
+        
+        data = ["{0:b}".format(item).zfill(self.counterSize) for item in data]
+        for i in range(0, len(data), 2):
+            char = list(self.binaryMap.keys())[list(self.binaryMap.values()).index(data[i + 1])]
+            result.append(chr(char) * (int(data[i], 2) + 1))
 
-        for i in range(0, len(self.encoded), 2):
-            char = list(self.binaryMap.keys())[list(self.binaryMap.values()).index(self.encoded[i + 1])]
-            result.append(char * (int(self.encoded[i], 2) + 1))
-
-        return "".join(result) 
+        if output_file_path:
+            try:
+                with open(output_file_path, 'w') as output_file:
+                    output_file.write("".join(result))
+                    print('File was decompressed successfully and saved to output path ...')
+                    return None 
+            except IOError:
+                print('Could not write to output file path. Please check if the path is correct ...')
+                raise 
+        return "".join(result)
     
