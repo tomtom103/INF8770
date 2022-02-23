ccc2f4ee19b5576facb7ca4fb6a6c4a621fa3f0d
Thomas Caron <thomas.caron@polymtl.ca>
2022-02-23 09:14:04 -0500

    TP2

--- TP2/main.py
+++ TP2/main.py
@@ -1,35 +1,37 @@
 #JPEG and JPEG2000
 from PIL import Image
-
+from skimage.metrics import structural_similarity as compare_ssim
 from pathlib import Path
 import cv2
-
-# def png_to_jpeg2000(path: Union[Path, str]):
-#     """
-#     Convert a PNG image to a JPEG2000 image.
-#     """
-#     img = Image.open(path)
-#     img.convert("RGB").save(path.with_suffix('.jp2'), 'JPEG2000', quality_mode='rates', quality_layers=[30])
-
-# def png_to_jpg(path: Union[Path, str]):
-#     """
-#     Convert a PNG image to a JPEG image.
-#     """
-#     img = Image.open(path)
-#     img.convert("RGB").save(path.with_suffix('.jpg'), 'JPEG')
 
 if __name__ == "__main__":
     # Get current file path
     current_path = Path(__file__).parent.absolute()
     img = Image.open(current_path / './assets/almonds.png')
     img.convert("RGB").save(current_path / './assets/full_quality_jpeg2000.jp2', 'JPEG2000')
-    img.convert("RGB").save(current_path / './assets/compressed_jpeg2000.jp2', 'JPEG2000', quality_mode='rates', quality_layers=[9])
+    img.convert("RGB").save(current_path / './assets/compressed_jpeg2000.jp2', 'JPEG2000', quality_mode='dB', quality_layers=[27])
 
     img.convert("RGB").save(current_path / './assets/full_quality_jpeg.jpg', 'JPEG', quality=95)
-    img.convert("RGB").save(current_path / './assets/compressed_jpeg.jpg', 'JPEG', quality=85)
+    img.convert("RGB").save(current_path / './assets/compressed_jpeg.jpg', 'JPEG', quality=38)
     im1 = cv2.imread('./TP2/assets/compressed_jpeg2000.jp2')
     im2 = cv2.imread('./TP2/assets/full_quality_jpeg2000.jp2')
-    print("JPEG2000: ", cv2.PSNR(im1, im2))
+    print("JPEG2000 PSNR: ", cv2.PSNR(im1, im2))
+
+    gray1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+    (score, diff) = compare_ssim(gray1, gray2, full=True)
+    diff = (diff * 255).astype("uint8")
+    print("JPEG2000 SSIM: {}".format(score))
+
+
     im1 = cv2.imread('./TP2/assets/full_quality_jpeg.jpg')
     im2 = cv2.imread('./TP2/assets/compressed_jpeg.jpg')
-    print("JPEG: ", cv2.PSNR(im1, im2))
+    print("JPEG PSNR: ", cv2.PSNR(im1, im2))
+
+    gray1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+    (score, diff) = compare_ssim(gray1, gray2, full=True)
+    diff = (diff * 255).astype("uint8")
+    print("JPEG SSIM: {}".format(score))
