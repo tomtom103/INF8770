e5478ea613bcff7d813193ba3071ef783e483b39
Thomas Caron <thomas.caron@polymtl.ca>
2022-04-06 10:06:04 -0400

    Completed code TP3

--- TP3/main.py
+++ TP3/main.py
@@ -1,14 +1,20 @@
-import enum
 import sys
-from typing import Literal
 import cv2
+from enum import Enum
+import statistics
 
+import numpy as np
+
+from typing import Dict, List, TypedDict
 from pathlib import Path
-from matplotlib import pyplot as plt
 
-class FrameType(enum.Enum):
-    CUT = 1
-    FADE = 2
+class FrameType(Enum):
+    CUT = 0
+    FADE = 1
+
+class Frame(TypedDict):
+    frame: int
+    type: FrameType
 
 def main():
     video_path = Path(__file__).parent / './assets/ski_cross.mp4'
@@ -19,9 +25,11 @@ def main():
         print('Error: cannot open video file')
         sys.exit(1)
 
-    current_frame = last_frame = None
+    current_frame: np.ndarray = None
+    last_frame: np.ndarray = None
 
-    frames = []
+    result: List[List[int]] = []
+    frames: List[Frame] = []
 
     while True:
         last_frame = current_frame
@@ -39,7 +47,9 @@ def main():
         cv2.imshow('frame', current_frame)
         frame_number = capture.get(cv2.CAP_PROP_POS_FRAMES)
 
-        comps = []
+        
+        histogram_comparisons = []
+
         if last_frame is not None:
             for x in range(0, width, width // 2):
                 for y in range(0, height, height // 2):
@@ -55,23 +65,29 @@ def main():
                     last_hist = cv2.calcHist([last_scope], [0], None, [256], [0, 256])
                     last_hist = cv2.normalize(last_hist, last_hist).flatten()
 
-                    comp = cv2.compareHist(current_hist, last_hist, cv2.HISTCMP_BHATTACHARYYA)
+                    comparison = cv2.compareHist(current_hist, last_hist, cv2.HISTCMP_BHATTACHARYYA)
 
-                    comps.append(comp)
+                    histogram_comparisons.append(comparison)
             
-            average = float(sum(comps)) / float(len(comps))
+            average = statistics.mean(histogram_comparisons)
 
             if average > 0.3:
-                if not frames:
-                    frames.append({ 'frame': frame_number, 'type': FrameType.CUT })
+                if not result:
+                    frame = Frame(frame=frame_number, type=FrameType.CUT)
+                    frames.append(frame)
+                    result.append([frame_number])
                 else:
                     if abs(frame_number - frames[-1]['frame']) > 10:
-                        frames.append({ 'frame': frame_number, 'type': FrameType.CUT })
+                        result.append([frame_number])
+                        frame = Frame(frame=frame_number, type=FrameType.CUT)
+                        frames.append(frame)
                     else:
+                        result[-1].append(frame_number)
                         frames[-1]['type'] = FrameType.FADE
-                        frames.append({ 'frame': frame_number, 'type': FrameType.FADE })
+                        frame = Frame(frame=frame_number, type=FrameType.FADE)
+                        frames.append(frame)
             
-            comps = []
+            histogram_comparisons.clear()
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -80,8 +96,13 @@ def main():
 
     cv2.destroyAllWindows()
 
-    for frame in frames:
-        print(f'Frame : {frame["frame"]} - Type: {frame["type"]}')
+    print(result)
+
+    for frame in result:
+        if len(frame) > 1:
+            print(f"Fondu: {min(frame)} - {max(frame)}")
+        else:
+            print(f"Coupure: {frame[0]}")
 
 if __name__ == '__main__':
     main()
